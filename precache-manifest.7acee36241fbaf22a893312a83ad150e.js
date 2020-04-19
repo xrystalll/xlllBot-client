e89af264365be7d287b56ec88fb7a307d10a18c6
xrystalll <mems.lark@gmail.com>
2020-04-19 15:38:11 +0300

    Updates

---
+++ precache-manifest.7acee36241fbaf22a893312a83ad150e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58829447a9663e69ae2c8d156f0ed82d",
+    "url": "/xlllBot-client/index.html"
+  },
+  {
+    "revision": "24a8e2fc0f19fd676b2b",
+    "url": "/xlllBot-client/static/css/2.6010d4dc.chunk.css"
+  },
+  {
+    "revision": "7e34d70fa34ae71379a2",
+    "url": "/xlllBot-client/static/css/main.cc854dca.chunk.css"
+  },
+  {
+    "revision": "24a8e2fc0f19fd676b2b",
+    "url": "/xlllBot-client/static/js/2.f1c5d137.chunk.js"
+  },
+  {
+    "revision": "584bdac8e4540382c06594ca076ca476",
+    "url": "/xlllBot-client/static/js/2.f1c5d137.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e34d70fa34ae71379a2",
+    "url": "/xlllBot-client/static/js/main.138f60c6.chunk.js"
+  },
+  {
+    "revision": "842724a11f3ba6122c48",
+    "url": "/xlllBot-client/static/js/runtime-main.ae88a14d.js"
+  },
+  {
+    "revision": "a26a488ad9bddb9ac95231aa1bacb9cd",
+    "url": "/xlllBot-client/static/media/twitch-background.a26a488a.jpg"
+  }
+]);
