51e9a80f40f591298088adf2ef9943acd82f60ad
xrystalll <mems.lark@gmail.com>
2020-04-22 16:13:53 +0300

    Updates

--- precache-manifest.d0ce64f377846297238298cecdb84d9b.js
+++ precache-manifest.d0ce64f377846297238298cecdb84d9b.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9801e2a4d88b32c9fd134634815c58ba",
+    "revision": "24296b9ac343e9dfce9ad5d105060789",
     "url": "/index.html"
   },
   {
-    "revision": "d41807879b2dcceaa33d",
-    "url": "/static/css/main.26b42e59.chunk.css"
+    "revision": "7af60e3e950c86cc8dc8",
+    "url": "/static/css/main.67c73bc3.chunk.css"
   },
   {
-    "revision": "6776828f9fe1ffedd386",
-    "url": "/static/js/2.7321a593.chunk.js"
+    "revision": "2a9d4885bc86797267bf",
+    "url": "/static/js/2.97bceff9.chunk.js"
   },
   {
     "revision": "584bdac8e4540382c06594ca076ca476",
-    "url": "/static/js/2.7321a593.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.97bceff9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d41807879b2dcceaa33d",
-    "url": "/static/js/main.16bd2264.chunk.js"
+    "revision": "7af60e3e950c86cc8dc8",
+    "url": "/static/js/main.7b8ca04a.chunk.js"
   },
   {
     "revision": "fc0f37c961bd7359fc00",
