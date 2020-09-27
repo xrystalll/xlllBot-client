73610df2f22341762c6e5f4452d15c592b3ee794
xrystalll <mems.lark@gmail.com>
2020-09-27 16:55:25 +0300

    Updates

---
+++ precache-manifest.caf25744f1e0b0083785ba383678403d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac8d9daa08afed94cfd348a9eccf5a1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "49682f0c399946d08a85",
+    "url": "./static/css/main.c1c66ae6.chunk.css"
+  },
+  {
+    "revision": "808bcdbbb5e406b42cf9",
+    "url": "./static/js/2.7d0d01de.chunk.js"
+  },
+  {
+    "revision": "c466c9f9ca8a3952aa9d29a8616621c0",
+    "url": "./static/js/2.7d0d01de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49682f0c399946d08a85",
+    "url": "./static/js/main.e471c04d.chunk.js"
+  },
+  {
+    "revision": "995bb1f5d8687c23d51c",
+    "url": "./static/js/runtime-main.7d88e8e0.js"
+  },
+  {
+    "revision": "a26a488ad9bddb9ac95231aa1bacb9cd",
+    "url": "./static/media/twitch-background.a26a488a.jpg"
+  }
+]);
