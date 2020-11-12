0e9c737e7403e7182fc2b012ba3ba06fbb3658b0
xrystalll <mems.lark@gmail.com>
2020-11-12 13:56:04 +0300

    Create mini player

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { getCookie } from 'components/support/Utils';
 import { socket } from 'instance/Socket';
+import { Store, StoreContext } from 'store/Store';
 import CustomScrollbar from 'components/support/CustomScrollbar';
 import PrivateRoute from 'components/support/PrivateRoute';
 import GeneralRoute from 'components/support/GeneralRoute';
@@ -16,9 +18,37 @@ import { Auth } from 'components/auth';
 import { AuthError } from 'components/auth/error';
 import { NotFound } from 'components/error';
 import { ToastContainer, toast } from 'react-toastify';
+import YouTubePlayer from 'components/videos/YoutubePlayer';
 
 class App extends Component {
+  static contextType = StoreContext;
   componentDidMount() {
+    if (!!getCookie('login') && !!getCookie('token')) {
+      this.subscribeToEvents()
+    }
+  }
+
+  subscribeToEvents() {
+    socket.on('output_videos', (data) => {
+      if (data.length > 0) {
+        this.context.dispatch({ type: 'SET_VIDEOS', payload: data })
+      } else {
+        this.context.dispatch({ type: 'SET_ERROR', payload: true })
+      }
+    })
+    socket.on('new_video', (data) => {
+      if (data.channel !== getCookie('login')) return
+
+      this.context.dispatch({ type: 'ADD_VIDEO', payload: data })
+      this.context.dispatch({ type: 'SET_ERROR', payload: false })
+      })
+    socket.on('deteted', (data) => {
+      this.context.dispatch({ type: 'REMOVE_VIDEO', payload: data })
+
+      if (this.context.state.response.filter(item => item._id !== data.id).length === 0) {
+        this.context.dispatch({ type: 'SET_ERROR', payload: true })
+      }
+    })
     socket.on('alert', (data) => {
       toast.error(data.message)
     })
@@ -44,9 +74,21 @@ class App extends Component {
         </Router>
 
         <ToastContainer position="bottom-right" autoClose={2000} pauseOnFocusLoss={false} />
+
+        {this.context.state.mini && <YouTubePlayer />}
       </CustomScrollbar>
     )
   }
 }
 
-export default App;
+class Root extends Component {
+  render() {
+    return (
+      <Store>
+        <App />
+      </Store>
+    )
+  }
+}
+
+export default Root;
